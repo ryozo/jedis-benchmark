6e0bec36ae0822af3a09ba8ab4886dbd1dbc3bbe
Ryozo Watanabe <clearstor@gmail.com>
2016-02-08 19:09:34 +0900

    When the app is launched, settings is logged.

--- src/main/java/net/equj65/jedisbench/Main.java
+++ src/main/java/net/equj65/jedisbench/Main.java
@@ -1,15 +1,29 @@
 package net.equj65.jedisbench;
 
 import com.beust.jcommander.JCommander;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author ryozo
  */
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
     public static void main(String[] args) {
+        logger.info("jedis-benchmark start.");
+
         CommandLineParameter param = new CommandLineParameter();
         new JCommander(param, args);
+
+        logger.info("[ Benchmark settings ]");
+        logger.info("  Server hostname : {}", param.getHostname());
+        logger.info("  Server port : {}", param.getPort());
+        logger.info("  Number of parallel connections : {}", param.getThreads());
+        logger.info("  Data size of SET/GET value in bytes : {}", param.getDataSize());
+        logger.info("  Total number of requests : {}", param.getRequests());
 
         Benchmarker benchmarker = new Benchmarker(param.getHostname(),
                 param.getPort(), param.getThreads(), param.getDataSize(), param.getRequests());

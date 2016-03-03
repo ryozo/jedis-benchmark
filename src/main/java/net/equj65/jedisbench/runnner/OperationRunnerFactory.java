81a2ea598828038296613616c1a1ec5388470410
Ryozo Watanabe <clearstor@gmail.com>
2016-03-03 21:39:05 +0900

    Repair the OperationRunnerFacotry for Decorator.

--- src/main/java/net/equj65/jedisbench/runnner/OperationRunnerFactory.java
+++ src/main/java/net/equj65/jedisbench/runnner/OperationRunnerFactory.java
@@ -5,10 +5,10 @@ import net.equj65.jedisbench.enums.Command;
 import net.equj65.jedisbench.generator.KeyGenerator;
 import net.equj65.jedisbench.generator.impl.FixedKeyGenerator;
 import net.equj65.jedisbench.generator.impl.RandomKeyGenerator;
-import net.equj65.jedisbench.mediator.StartSignal;
 import net.equj65.jedisbench.runnner.impl.GetOperationRunner;
 import net.equj65.jedisbench.runnner.impl.SetOperationRunner;
-import net.equj65.jedisbench.runnner.impl.WaitReplicasSetOperationRunner;
+import net.equj65.jedisbench.runnner.impl.WaitReplicasOperationRunner;
+import net.equj65.jedisbench.util.ReplicationUtils;
 import redis.clients.jedis.JedisPool;
 
 import java.io.StringWriter;
@@ -16,28 +16,50 @@ import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
 
 /**
- * Created by ryozo on 2016/02/08.
+ * This class is factory of {@link OperationRunner} class.
+ *
+ * @author ryozo
  */
 public class OperationRunnerFactory {
 
-    // TODO refactor
+    /**
+     * Create a new instance of the {@link OperationRunner} to run the specified benchmarks.
+     *
+     * @param command RedisCommand of benchmark target.
+     * @param context Condition of benchmark.
+     * @param pool The JedisPool instance for OperationRunner to use it.
+     * @param latch CountDownLatch that records the number of benchmark samples.
+     * @return created OperationRunner instance.
+     */
     public static OperationRunner createRunnerOf(Command command, BenchmarkContext context,
                                                  JedisPool pool, CountDownLatch latch) {
+        // TODO refactor
         KeyGenerator generator = createKeyGeneratorOf(context);
         String data = fillString(context.getDataSize());
-        switch (command) {
-            case SET:
-                if (isUseWaitReplicas(context)) {
-                    return new WaitReplicasSetOperationRunner(pool, latch, data, generator,
-                            context.getAcknowledgedReplicas(), context.getWaitTimeout());
-                }
-                return new SetOperationRunner(pool, latch, data, generator);
-            case GET:
-                return new GetOperationRunner(pool, latch, data, generator);
+
+        OperationRunner runner = null;
+        if (Command.SET == command) {
+            runner = new SetOperationRunner(pool, latch, data, generator);
+        } else if (Command.GET == command) {
+            runner = new GetOperationRunner(pool, latch, data, generator);
+        } else {
+            throw new IllegalArgumentException(String.format("Command [%s] is not supported.", command));
         }
-        throw new IllegalArgumentException(String.format("Command [%s] is not supported.", command));
+
+        // decorate
+        if (isUseWaitReplicas(command, context)) {
+            return new WaitReplicasOperationRunner(runner, pool,
+                    context.getAcknowledgedReplicas(), context.getWaitTimeout());
+        }
+        return runner;
     }
 
+    /**
+     * Create a {@link KeyGenerator} from specified {@link BenchmarkContext}.
+     *
+     * @param context
+     * @return
+     */
     private static KeyGenerator createKeyGeneratorOf(BenchmarkContext context) {
         if (1 < context.getKeySpaceLen()) {
             return new RandomKeyGenerator(context.getKeySpaceLen());
@@ -57,8 +79,12 @@ public class OperationRunnerFactory {
         }}.toString();
     }
 
-    private static boolean isUseWaitReplicas(BenchmarkContext context) {
-        return 1 <= context.getAcknowledgedReplicas();
+    /**
+     * Confirm whether specified Command and BenchmarkContext are a target of WaitReplicas.
+     */
+    private static boolean isUseWaitReplicas(Command command, BenchmarkContext context) {
+        return ReplicationUtils.isWaitableCommand(command)
+                && 1 <= context.getAcknowledgedReplicas();
     }
 
 }
